29a34beec84011361489aba9125d014740f4fdd6
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-15 18:48:06 +0530

    database setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("Signup route");
+};
+export const login = (req, res) => {
+    res.send("Login route");
+  };
+  export const logout = (req, res) => {
+    res.send("Logout route");
+  };
